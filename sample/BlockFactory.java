0b2155d5de1af556dbffcc029298a38c9f15ea77
BGH76 <75952591+BGH76@users.noreply.github.com>
2022-07-31 14:06:34 -0400

    initial

---
+++ sample/BlockFactory.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class BlockFactory {
+
+    // Todo: Needs to randomly select shape and place blocks into activeBlockList.
+    // Todo: Starting positions will depend on Block Shape.
+}
