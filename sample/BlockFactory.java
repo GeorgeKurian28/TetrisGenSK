969113ca423830a574a9b14db24c4aae087f54df
Sean Gates <107445149+seanx63@users.noreply.github.com>
2022-08-01 07:49:40 -0700

    Delete sample directory

--- sample/BlockFactory.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class BlockFactory {
-
-    // Todo: Needs to randomly select shape and place blocks into activeBlockList.
-    // Todo: Starting positions will depend on Block Shape.
-}
